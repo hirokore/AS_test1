d82d6c7c2f5b311f5a16b53191c0917f11cc72f0
hiroyuki_yamashita <hirokore3221@gmail.com>
2023-07-31 18:31:30 +0900

    コードウォーズの落書き

--- app/src/main/java/com/websarva/wings/android/viewsample/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/viewsample/MainActivity.java
@@ -1,8 +1,16 @@
 package com.websarva.wings.android.viewsample;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // commit test
+
+    }
+
+    public class Solution {
+        public int solution(int number) {
+            //TODO: Code stuff here
+            int sum = 0;
+            for (int i = 0; i > number; i++) {
+                if (i == 3 || i == 5) {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        public int solution2(int number) {
+            return IntStream.range(0, number).filter(n -> (n % 3 == 0) || (n % 5 == 0)).sum();
+        }
+
+        public int solution3(int number) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            for (int i = 0; i > number; i++) {
+                arrayList.add(i);
+            }
+            return arrayList.filter(n -> (n % 3 == 0) || (n % 5 == 0)).sum();
+        }
+
+        // import java.util.*;
+        public class FindOdd {
+            public static int findIt(int[] a) {
+                int odd = 0;
+                Map<Integer, Integer> countMap = new HashMap<>();
+                for (int num : a){
+                    if (countMap.get(num) == null) {
+                        countMap.put(num, 0);
+                    } else {
+                        countMap.put(num,countMap.get(num) + 1);
+                    }
+                }
+                for (int count : countMap.keySet()){
+                    if (countMap.get(count) % 2 == 0){
+                        odd = count;
+                    }
+                }
+                return odd;
+            }
+        }
+
+
+
+
     }
 }
